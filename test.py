30ceabd9042ac9828fbcdbd684d69533c59b7c5b
pavan3175 <pavankumar3130@gmail.com>
2021-10-16 20:57:30 -0700

    First Commit

---
+++ test.py
@@ -0,0 +1,31 @@
+def add(a,b):
+    sum=a+b
+    print(sum)
+    return sum
+
+
+def mul(a,b):
+    mul = a*b
+    print(mul)
+    return mul
+
+
+def mod1(a,b):
+    mod1 = a%b
+    print(mod1)
+    return mod1
+
+
+
+print("Declaring two number,This is the first number")
+a = 10
+print("Declaring two number,This is the first number")
+b = 10
+sum_result = add(a,b)
+print(sum_result)
+mul_result = mul(a,b)
+print(mul_result)
+mod1_result = mod1(a,b)
+print(mod1_result)
+
+
